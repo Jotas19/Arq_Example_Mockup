6508672ad9ba71a0513c7ffacb07c3c425629efb
Juan José Rojas Pérez <74082637+Jotas19@users.noreply.github.com>
2023-06-05 03:39:24 -0500

    Add files via upload

---
+++ Ecommerce3.Negocio/ImagenProducto_Negocio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ecommerce.Datos;
+using Ecommerce3.Dominioo;
+
+namespace Ecommerce3.Negocio
+{
+    public class ImagenProducto_Negocio
+    {
+        private Producto objImagenProductoDTO = new Producto();
+
+        public List<ImagenProductoDTO> Listar_Imagen()
+        {
+            return objImagenProductoDTO.Listar_Imagen();
+        }
+    }
+}
